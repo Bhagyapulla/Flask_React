c64be844fe778f13de682d4298f6d50692465d62
Bhagyapulla <pullabhagyalakshmi6@gmail.com>
2025-09-19 08:19:14 +0530

    add app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def home():
     return "✅ Flask Backend is Running!"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+
+    port = int(os.environ.get("PORT", 5000))  # Render gives PORT, else fallback 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
